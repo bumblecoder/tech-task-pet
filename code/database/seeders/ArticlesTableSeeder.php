7b19befb0e708b7746b1664558ef3d0ec4c765ea
Anton Bielykh <abeelykh@gmail.com>
2024-07-31 10:23:24 -0600

    image url

--- code/database/seeders/ArticlesTableSeeder.php
+++ code/database/seeders/ArticlesTableSeeder.php
@@ -12,16 +12,16 @@ final class ArticlesTableSeeder extends Seeder
     public function run(): void
     {
         DB::table('articles')->insert([
-            ['title' => 'First Article', 'content' => 'Content of the first article', 'created_at' => now()],
-            ['title' => 'Second Article', 'content' => 'Content of the second article', 'created_at' => now()],
-            ['title' => 'Third Article', 'content' => 'Content of the third article', 'created_at' => now()],
-            ['title' => 'Fourth Article', 'content' => 'Content of the fourth article', 'created_at' => now()],
-            ['title' => 'Fifth Article', 'content' => 'Content of the fifth article', 'created_at' => now()],
-            ['title' => 'Sixth Article', 'content' => 'Content of the sixth article', 'created_at' => now()],
-            ['title' => 'Seventh Article', 'content' => 'Content of the seventh article', 'created_at' => now()],
-            ['title' => 'Eighth Article', 'content' => 'Content of the eighth article', 'created_at' => now()],
-            ['title' => 'Ninth Article', 'content' => 'Content of the ninth article', 'created_at' => now()],
-            ['title' => 'Tenth Article', 'content' => 'Content of the tenth article', 'created_at' => now()],
+            ['title' => 'First Article', 'content' => 'Content of the first article', 'created_at' => now(), 'image' => 'https://d15ywwv3do91l7.cloudfront.net/medium.webp'],
+            ['title' => 'Second Article', 'content' => 'Content of the second article', 'created_at' => now(), 'image' => 'https://d15ywwv3do91l7.cloudfront.net/medium.webp'],
+            ['title' => 'Third Article', 'content' => 'Content of the third article', 'created_at' => now(), 'image' => 'https://d15ywwv3do91l7.cloudfront.net/medium.webp'],
+            ['title' => 'Fourth Article', 'content' => 'Content of the fourth article', 'created_at' => now(), 'image' => 'https://d15ywwv3do91l7.cloudfront.net/medium.webp'],
+            ['title' => 'Fifth Article', 'content' => 'Content of the fifth article', 'created_at' => now(), 'image' => 'https://d15ywwv3do91l7.cloudfront.net/medium.webp'],
+            ['title' => 'Sixth Article', 'content' => 'Content of the sixth article', 'created_at' => now(), 'image' => 'https://d15ywwv3do91l7.cloudfront.net/medium.webp'],
+            ['title' => 'Seventh Article', 'content' => 'Content of the seventh article', 'created_at' => now(), 'image' => 'https://d15ywwv3do91l7.cloudfront.net/medium.webp'],
+            ['title' => 'Eighth Article', 'content' => 'Content of the eighth article', 'created_at' => now(), 'image' => 'https://d15ywwv3do91l7.cloudfront.net/medium.webp'],
+            ['title' => 'Ninth Article', 'content' => 'Content of the ninth article', 'created_at' => now(), 'image' => 'https://d15ywwv3do91l7.cloudfront.net/medium.webp'],
+            ['title' => 'Tenth Article', 'content' => 'Content of the tenth article', 'created_at' => now(), 'image' => 'https://d15ywwv3do91l7.cloudfront.net/medium.webp'],
         ]);
     }
 }
